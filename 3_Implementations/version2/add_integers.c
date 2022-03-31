605c3808d9e507ee1a8fbbebf7562b73f0b80122
divyanshu singh <82446712+divyanshusingh700@users.noreply.github.com>
2022-03-31 23:54:22 +0530

    Update add_integers.c

--- 3_Implementations/version2/add_integers.c
+++ 3_Implementations/version2/add_integers.c
@@ -1,35 +1,15 @@
-/**
- * @file add_integer.h
- * @author Bharath G 
- * @brief Interface for Adding two integer numbers with underflow and overflow checks
- * @version 0.1
- * @date 2022-03-17
- * 
- * @copyright Copyright (c) 2022
- * 
- */
+#include "add_integer.h"
 
-#ifndef _ADD_INTEGER_H_
-#define _ADD_INTEGER_H_
-
-#include <stdio.h>
-#include <limits.h>
-
-typedef enum funtion_status_t
+funtion_status_t add_integers(int param1, int param2, int* result)
 {
-    SUCCESS,
-    OVERFLOW,
-    UNDERFLOW
-}funtion_status_t;
+    long int sum = param1 + param2;
 
-/**
- * @brief Function to add 2 integer numbers
- * 
- * @param param1[in] Operand 1 to be added
- * @param param2[in] Operand 2 to be added
- * @param result[out] Result of add operation if success
- * @return funtion_status_t Result of the function execution
- */
-funtion_status_t add_integers(int param1, int param2, int* result);
+    if(sum > INT_MAX)
+        return OVERFLOW;
 
-#endif  /* _ADD_INTEGER_H_ */
+    if(sum < INT_MIN)
+        return UNDERFLOW;
+    
+    *result = sum;
+    return SUCCESS;
+}
